77537c86c37cb23fa5822cf614a94378897db1ff
Justin Partain <justinwpartain1@gmail.com>
2016-10-14 16:28:28 -0400

    Starting on grabbing street blocks for building placement.

--- include/town.py
+++ include/town.py
@@ -25,13 +25,120 @@ class Town:
         self.generateBuildingRatios()
         self.createRoadsEquations(seed[25:126])
         self.placeRoads()
+        # self.createStreetBlocks(seed[126:176])
         self.placeBuildings()
+
+    def createOpinions(self, string):
+        pass
+
+    def createPerson(self, string):
+        pass
+
+    def createRoadsEquations(self, string):
+        self.horizontal_roads_m = []
+        self.horizontal_roads_b = []
+        self.vertical_roads_m = []
+        self.vertical_roads_b = []
+
+        map_width = self.y_max - self.y_min
+        map_height = self.x_max - self.x_min
+
+        median_map_width = 72
+        median_map_height = 72
+        median_road_num = 5
+
+        horizontal_roads_num = int(map_height / median_map_height *
+                                   median_road_num - ((int(string[0]) - 4.5) / 3))
+        vertical_roads_num =   int(map_width / median_map_width *
+                                   median_road_num - ((int(string[1]) - 4.5) / 3))
+
+        horizontal_road_space = int(map_height / (horizontal_roads_num + 1))
+        vertical_road_space =   int(map_width / (vertical_roads_num + 1))
+
+        for i in range(0, horizontal_roads_num - 2):
+            horizontal_slope =     ((int(string[i + 2]) - 4.5) / 18)
+            horizontal_intercept = (((int(string[i + 2 + horizontal_roads_num - 1])
+                                      - 4.5) / 36) + (i + 1) *
+                                    horizontal_road_space - map_height/2)
+
+            self.horizontal_roads_m.append(horizontal_slope)
+            self.horizontal_roads_b.append(horizontal_intercept)
+
+        for i in range(0, vertical_roads_num - 2):
+            inv_vertical_slope = ((int(string[i + 2 + horizontal_roads_num * 2 - 1])
+                                   - 4.5) / 18)
+            vertical_x_intercept = (((int(string[i + 2 + horizontal_roads_num * 2 -
+                                   1 + vertical_roads_num - 1]) - 4.5) / 36) +
+                                   (i + 1) * vertical_road_space - map_width/2)
+
+            if inv_vertical_slope == 0:
+                inv_vertical_slope = 0.1
+
+            vertical_slope = pow(inv_vertical_slope, -1)
+            vertical_intercept = -1 * vertical_x_intercept * vertical_slope
+
+            self.vertical_roads_m.append(vertical_slope)
+            self.vertical_roads_b.append(vertical_intercept)
+
+        # Map Edges
+        for slope, intercept in zip(self.draw_slopes, self.draw_intercepts):
+            if abs(slope) > 1:
+                self.vertical_roads_m.append(slope)
+
+                if intercept > 0:
+                    self.vertical_roads_b.append(intercept - 1)
+                else:
+                    self.vertical_roads_b.append(intercept + 1)
+
+            else:
+                self.horizontal_roads_m.append(slope)
+
+                if intercept < 0:
+                    self.horizontal_roads_b.append(intercept - 1)
+                else:
+                    self.horizontal_roads_b.append(intercept)
+
+    def createStreetBlocks(self, string):
+        self.street_block_list = []
+
+        # for point in self.map_points:
+        pass
+
+    def generateBuildingRatios(self):
+        area_mod = self.wealth / 25
+        map_unit_area = 5256
+
+        self.tavern_area =      30 * (1 + area_mod) * (self.map_area/map_unit_area)
+        self.plumbing_area =    20 * (1 + area_mod) * (self.map_area/map_unit_area)
+        self.market_area =      100 * (1 + area_mod) * (self.map_area/map_unit_area)
+        self.trade_area =       60 * (1 + area_mod) * (self.map_area/map_unit_area)
+        self.inn_area =         30 * (1 + area_mod) * (self.map_area/map_unit_area)
+        self.special_area =     100 * (1 + area_mod) * (self.map_area/map_unit_area)
+
+        housing_area =  int(self.map_area - self.tavern_area - self.plumbing_area -
+                            self.market_area - self.trade_area - self.inn_area -
+                            self.special_area)
+
+        self.number_noble_house =  int(housing_area * (0.2 + self.wealth/25))
+        self.number_middle_house = int(housing_area * (0.4 + self.wealth/25))
+        self.number_poor_house =   (housing_area - self.number_noble_house -
+                                    self.number_middle_house)
+
+        """
+        print()
+        print('Noble area:\t', self.number_noble_house, '\t',
+              int(self.number_noble_house/housing_area * 100), '%')
+
+        print('Middle area:\t', self.number_middle_house, '\t',
+              int(self.number_middle_house/housing_area * 100), '%')
+
+        print('Poor area:\t', self.number_poor_house, '\t',
+              int(self.number_poor_house/housing_area * 100), '%')
+        print()
+        """
 
     def generatePopulation(self, seed):
         pass
-
-    def generateWealth(self, string):
-        self.wealth = int(string) - 5
 
     def generateEconomy(self, string):
         self.economy = TownEconomy[int(string)]
@@ -47,16 +154,6 @@ class Town:
             self.danger_mod = 1
 
         self.danger = int(self.danger_mod*int(string))
-
-    def generateNobility(self, string):
-        if self.economy == 'Church':
-            self.nobility_mod = 0.5
-        elif self.economy == 'Military':
-            self.nobility_mod = 0.4
-        else:
-            self.nobility_mod = 0.35
-
-        self.nobility = int(10*self.nobility_mod*int(string))
 
     def generateHomeless(self, string):
         self.settled_ratio = int(10*(int(string)/5 + self.wealth + 5))
@@ -320,69 +417,29 @@ class Town:
                         self.map_points.append(point)
         self.map_area = len(self.map_points)
 
-    def createRoadsEquations(self, string):
-        self.horizontal_roads_m = []
-        self.horizontal_roads_b = []
-        self.vertical_roads_m = []
-        self.vertical_roads_b = []
+    def generateNobility(self, string):
+        if self.economy == 'Church':
+            self.nobility_mod = 0.5
+        elif self.economy == 'Military':
+            self.nobility_mod = 0.4
+        else:
+            self.nobility_mod = 0.35
 
-        map_width = self.y_max - self.y_min
-        map_height = self.x_max - self.x_min
+        self.nobility = int(10*self.nobility_mod*int(string))
 
-        median_map_width = 72
-        median_map_height = 72
-        median_road_num = 5
+    def generateWealth(self, string):
+        self.wealth = int(string) - 5
 
-        horizontal_roads_num = int(map_height / median_map_height *
-                                   median_road_num - ((int(string[0]) - 4.5) / 3))
-        vertical_roads_num =   int(map_width / median_map_width *
-                                   median_road_num - ((int(string[1]) - 4.5) / 3))
+    def linkRelationships(self, string):
+        pass
 
-        horizontal_road_space = int(map_height / (horizontal_roads_num + 1))
-        vertical_road_space =   int(map_width / (vertical_roads_num + 1))
+    def placeBuildings(self):
+        # self.building_list = []
+        # self.building_list.append(Building(string))
 
-        for i in range(0, horizontal_roads_num - 2):
-            horizontal_slope =     ((int(string[i + 2]) - 4.5) / 18)
-            horizontal_intercept = (((int(string[i + 2 + horizontal_roads_num - 1])
-                                      - 4.5) / 36) + (i + 1) *
-                                    horizontal_road_space - map_height/2)
-
-            self.horizontal_roads_m.append(horizontal_slope)
-            self.horizontal_roads_b.append(horizontal_intercept)
-
-        for i in range(0, vertical_roads_num - 2):
-            inv_vertical_slope = ((int(string[i + 2 + horizontal_roads_num * 2 - 1])
-                                   - 4.5) / 18)
-            vertical_x_intercept = (((int(string[i + 2 + horizontal_roads_num * 2 -
-                                   1 + vertical_roads_num - 1]) - 4.5) / 36) +
-                                   (i + 1) * vertical_road_space - map_width/2)
-
-            if inv_vertical_slope == 0:
-                inv_vertical_slope = 0.1
-
-            vertical_slope = pow(inv_vertical_slope, -1)
-            vertical_intercept = -1 * vertical_x_intercept * vertical_slope
-
-            self.vertical_roads_m.append(vertical_slope)
-            self.vertical_roads_b.append(vertical_intercept)
-
-        # Map Edges
-        for slope, intercept in zip(self.draw_slopes, self.draw_intercepts):
-            if abs(slope) > 1:
-                self.vertical_roads_m.append(slope)
-
-                if intercept > 0:
-                    self.vertical_roads_b.append(intercept - 1)
-                else:
-                    self.vertical_roads_b.append(intercept + 1)
-
-            else:
-                self.horizontal_roads_m.append(slope)
-
-                if intercept < 0:
-                    self.horizontal_roads_b.append(intercept - 1)
-                else:
-                    self.horizontal_roads_b.append(intercept)
+        # generate size
+        # generate building types for each square
+        pass
 
     def placeRoads(self):
         for point in self.map_points:
@@ -398,56 +455,6 @@ class Town:
                     (point.x == int((point.y - v_int) / v_slope) + 1)):
                         point.building = 'Road'
                         break
-
-    def generateBuildingRatios(self):
-        area_mod = self.wealth / 25
-        map_unit_area = 5256
-
-        self.tavern_area =      30 * (1 + area_mod) * (self.map_area/map_unit_area)
-        self.plumbing_area =    20 * (1 + area_mod) * (self.map_area/map_unit_area)
-        self.market_area =      100 * (1 + area_mod) * (self.map_area/map_unit_area)
-        self.trade_area =       60 * (1 + area_mod) * (self.map_area/map_unit_area)
-        self.inn_area =         30 * (1 + area_mod) * (self.map_area/map_unit_area)
-        self.special_area =     100 * (1 + area_mod) * (self.map_area/map_unit_area)
-
-        housing_area =  int(self.map_area - self.tavern_area - self.plumbing_area -
-                            self.market_area - self.trade_area - self.inn_area -
-                            self.special_area)
-
-        self.number_noble_house =  int(housing_area * (0.2 + self.wealth/25))
-        self.number_middle_house = int(housing_area * (0.4 + self.wealth/25))
-        self.number_poor_house =   (housing_area - self.number_noble_house -
-                                    self.number_middle_house)
-
-        """
-        print()
-        print('Noble area:\t', self.number_noble_house, '\t',
-              int(self.number_noble_house/housing_area * 100), '%')
-
-        print('Middle area:\t', self.number_middle_house, '\t',
-              int(self.number_middle_house/housing_area * 100), '%')
-
-        print('Poor area:\t', self.number_poor_house, '\t',
-              int(self.number_poor_house/housing_area * 100), '%')
-        print()
-        """
-
-    def placeBuildings(self):
-        # self.building_list = []
-        # self.building_list.append(Building(string))
-
-        # generate size
-        # generate building types for each square
-        pass
-
-    def createPerson(self, string):
-        pass
-
-    def linkRelationships(self, string):
-        pass
-
-    def createOpinions(self, string):
-        pass
 
     def printMapCorners(self):
         # print('Map corner points:')
